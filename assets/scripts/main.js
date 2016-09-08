6d247e272115ed9be4a6c09b9d0745fb7f8a2ccf
LuisMiguel <LuisMiguel>
2016-09-08 22:35:19 +0100

    portfolio com template

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,34 +1,39 @@
+$(function() {
 
-    
-    
-var gallery = $(".js-gallery");
-var gallery_featured = $(".js-gallery-featured");
-
-var work_item = $('.work');
-var portfolio = $('.portfolio');
- 
- $('.close-buttom').click(function(e) {
-        hideWork();
-});
-
-
-$(document).click(function(e) {
-        if (
-                $(e.target).is(".showcase") === false &&
-                $(e.target).is(".showcase *") === false &&
-                $(e.target).is(".work") === false &&
-                $(e.target).is(".work *") === false
-                ) {
+        $.getJSON('/data/portfolio.json', function(data) {
+                var template = $('#works-template').html();
+                Mustache.parse(template);   // optional, speeds up future uses
+                var rendered = Mustache.render(template, data);
+                $('#works').html(rendered);
+        });
+        $(document).on("click", ".close-buttom", function() {
                 hideWork();
-        }
+        });
+        $(document).click(function(e) {
+                if (
+                        $(e.target).is(".showcase") === false &&
+                        $(e.target).is(".showcase *") === false &&
+                        $(e.target).is(".work") === false &&
+                        $(e.target).is(".work *") === false
+                        ) {
+                        hideWork();
+                }
+        });
+        $(document).on("click", ".work", function() {
+
+                var work_name = $(this).attr('work_name');
+                $('.showcase' + '[work_name=' + work_name + ']').addClass('show');
+                $('.portfolio').addClass('showcase_shown');
+
+        });
+
 });
 
-work_item.click(function(e) {
-        var work_name = $(this).attr('work_name');
-        $('.showcase' + '[work_name=' + work_name + ']').addClass('show');
-        portfolio.addClass('showcase_shown');
-});
+
 function hideWork() {
     $('.showcase').removeClass('show');
-    portfolio.removeClass('showcase_shown');
+    $('.portfolio').removeClass('showcase_shown');
+}
+function ola() {
+   console.log("ola");
 }
