d24590a7278cad903146fdbb0413e1c7b3b0d4a6
Luís Silva <luismiguel.s91@gmail.com>
2016-11-28 00:23:43 +0000

    refractoring javascript

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,17 +1,19 @@
-var a = 1;
-var template, rendered;
-
-function toggleWork(work_showcase) {
+function renderWorks() {
     'use strict';
-    if (work_showcase.hasClass('show')) {
-        work_showcase.removeClass('show');
-        $('.portfolio').removeClass('showcase_shown');
-    } else {
-        work_showcase.addClass('show');
-        $('.portfolio').removeClass('showcase_shown');
-    }
-
+    var template, rendered;
+    $.getJSON('/data/portfolio.json', function (data) {
+        if (data) {
+            for (var i = 0; i < data.works.length; i++) {
+                data.works[i].index = i;
+            }
+            template = $('#works-template').html();
+            Mustache.parse(template);
+            rendered = Mustache.render(template, data);
+            $('#works').html(rendered);
+        }
+    });
 }
+
 function hideWork() {
     'use strict';
     $('.work-showcase').removeClass('show');
@@ -20,26 +22,14 @@ function hideWork() {
 
 $(function () {
     'use strict';
-    $.getJSON('/data/portfolio.json', function (data) {
-        var i = 0;
-        if (data) {
-            for (i; i < data.works.length; i++) {
-                data.works[i].index = i;
-            }
-        }
+    renderWorks();
 
-        template = $('#works-template').html();
-        Mustache.parse(template);
-        rendered = Mustache.render(template, data);
-        $('#works').html(rendered);
-    });
     $(document).on("click", ".close-buttom", function () {
         hideWork();
     });
     $(document).click(function (e) {
-        if ($(e.target).is(".showcase") === false && $(e.target).is(".showcase *") === false &&
-                $(e.target).is(".work") === false && $(e.target).is(".work *") === false
-                ) {
+        if (    $(e.target).is(".work") === false && 
+                $(e.target).is(".work *") === false ) {
             hideWork();
         }
     });
@@ -47,6 +37,5 @@ $(function () {
         hideWork();
         var work_id = $(this).attr('work_id');
         $('.work-showcase' + '[work_id=' + work_id + ']').addClass('show');
-        $('.portfolio').addClass('showcase_shown');
     });
 });
