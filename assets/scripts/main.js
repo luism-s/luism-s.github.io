d2ad5555e8587424c653d11049f808c0e79f14a6
Luís Silva <luismiguel.s91@gmail.com>
2017-01-24 01:35:39 +0000

    Massive updates, I'm lazy

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,41 +1,41 @@
 function renderWorks() {
-    'use strict';
-    var template, rendered;
-    $.getJSON('/data/portfolio.json', function (data) {
-        if (data) {
-            for (var i = 0; i < data.works.length; i++) {
-                data.works[i].index = i;
-            }
-            template = $('#works-template').html();
-            Mustache.parse(template);
-            rendered = Mustache.render(template, data);
-            $('#works').html(rendered);
+  'use strict';
+  var template, rendered;
+  $.getJSON('/data/portfolio.json', function (data) {
+    if (data) {
+        for (var i = 0; i < data.works.length; i++) {
+            data.works[i].index = i;
         }
-    });
+        template = $('#works-template').html();
+        Mustache.parse(template);
+        rendered = Mustache.render(template, data);
+        $('#works').html(rendered);
+    }
+  });
 }
 
 function hideWork() {
-    'use strict';
-    $('.work-showcase').removeClass('show');
-    $('.portfolio').removeClass('showcase_shown');
+  'use strict';
+  $('.js-work-showcase').removeClass('shown');
 }
 
-$(function () {
-    'use strict';
-    renderWorks();
 
-    $(document).on("click", ".close-buttom", function () {
-        hideWork();
-    });
-    $(document).click(function (e) {
-        if (    $(e.target).is(".work") === false && 
-                $(e.target).is(".work *") === false ) {
-            hideWork();
-        }
-    });
-    $(document).on("click", ".work-box", function () {
-        hideWork();
-        var work_id = $(this).attr('work_id');
-        $('.work-showcase' + '[work_id=' + work_id + ']').addClass('show');
-    });
+$(function () {
+  'use strict';
+  renderWorks();
+
+  $(document).on("click", ".close-buttom", function () {
+    hideWork();
+  });
+  $(document).click(function (e) {
+    if (    $(e.target).is(".work") === false && 
+            $(e.target).is(".work *") === false ) {
+      hideWork();
+    }
+  });
+  $(document).on("click", ".js-work-preview", function () {
+    hideWork();
+    var work_id = $(this).attr('work_id');
+    $('.js-work-showcase' + '[work_id=' + work_id + ']').addClass('shown');
+  });
 });
