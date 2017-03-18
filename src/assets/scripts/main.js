a8941eafffe1cf96d33a64ad314efcee231a9135
Luís Silva <luismiguel.s91@gmail.com>
2017-03-18 23:38:55 +0000

    "A programmer that doesn't reinvent his own solutions is not a good programmer" - Confucius

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,91 +1,92 @@
-function workTracking(work_id, behaviour) {
-  var work_name = $('[work_id=' + work_id + '] .js-work-preview-title').html();
-  ga('send', 'event', 'Works', behaviour, work_name, work_id);
+function workTracking( work_id, behaviour ) {
+  var work_name = $( '[work_id=' + work_id + '] .js-work-preview-title' ).html();
+  ga( 'send', 'event', 'Works', behaviour, work_name, work_id );
 }
 
 function renderWorks() {
   'use strict';
   var template, rendered;
-  $.getJSON('/data/portfolio.json', function (data) {
-    if (data) {
-        for (var i = 0; i < data.works.length; i++) {
-            data.works[i].index = i;
-        }
-        template = $('#works-template').html();
-        Mustache.parse(template);
-        rendered = Mustache.render(template, data);
-        $('#works').html(rendered);
-        $('[work_id=0] .button__navigation--left').addClass('button__navigation--inactive');
-        $('[work_id=' + (data.works.length - 1) + '] .button__navigation--right').addClass('button__navigation--inactive');
+    $.getJSON( '/data/portfolio.json', function ( data ) {
+    if ( data ) {
+      
+      for ( var i = 0; i < data.works.length; i++ ) {
+        data.works[i].index = i;
+      }
+      template = $( '#works-template' ).html();
+      Mustache.parse( template );
+      rendered = Mustache.render( template, data );
+      
+      $('#works').html( rendered );
+      $('[work_id=0] .button__navigation.-left').addClass( '-inactive' );
+      $('[work_id=' + ( data.works.length - 1 ) + '] .button__navigation.-right').addClass( '-inactive' );
     }
-  });
+  } );
 }
 
-function hideWork(work_id) {
+function hideWork( work_id ) {
   'use strict';
-  if(work_id >= 0) {
-    $('[work_id=' + work_id + '] .js-work-showcase').removeClass('work-showcase--shown');
+  if ( work_id >= 0 ) {
+    $('[work_id=' + work_id + '] .js-work-showcase').removeClass( '-shown' );
   } else {
-    $('.js-work-showcase').removeClass('work-showcase--shown');
+    $('.js-work-showcase').removeClass( '-shown' );
   }
-  $('.dimmed_background').removeClass('dimmed_background--active');
+  $('.dimmed_background').removeClass( '-active' );
 }
 
-function showWork(work_id) {
+function showWork( work_id ) {
   var showcase = '[work_id=' + work_id + '] .js-work-showcase';
-  if( work_id >= 0 && !$(showcase).hasClass("work-showcase--shown") ) {
-    $(showcase).addClass('work-showcase--shown');
-    $('.dimmed_background').addClass('dimmed_background--active');
-    workTracking(work_id, 'view');
+  if ( work_id >= 0 && !$( showcase ).hasClass( "-shown" ) ) {
+    $(showcase).addClass( '-shown' );
+    $('.dimmed_background').addClass( '-active' );
+    workTracking( work_id, 'view' );
   }
 }
 
-function switchWork(shown_work_id, next_work_id) {
-  if(shown_work_id !== next_work_id) {
-    hideWork(shown_work_id);
-    showWork(next_work_id);
+function switchWork( shown_work_id, next_work_id ) {
+  if ( shown_work_id !== next_work_id ) {
+    hideWork( shown_work_id );
+    showWork( next_work_id );
   }
 }
 
-$(function () {
+$( function () {
   'use strict';
   renderWorks();
 
-  $(document).on("click", ".js-close-work", function () {
+  $( document ).on( 'click', '.js-close-work', function () {
     hideWork();
-  });
+  } );
 
-  $(document).click(function (e) {
-    if ( $(e.target).is(".work *") === false ) {
+  $( document ).click( function ( e ) {
+    if ( !$( e.target ).is( '.work *' ) ) {
       hideWork();
     }
   });
   
-  $(document).on("click", ".js-work-preview", function () {
-    var work_id = $(this).parent().attr('work_id');
+  $( document ).on( 'click', '.js-work-preview', function () {
+    var work_id = $(this).parent().attr( 'work_id' );
     hideWork();
-    showWork(work_id);
-    workTracking(work_id, 'click');
-  });
+    showWork( work_id );
+    workTracking( work_id, 'click' );
+  } );
 
 
-  $(document).on("click", ".js-switch-work", function () {
-    var work_id = Number($('.work-showcase--shown').parent().attr('work_id'));
+  $( document ).on( 'click', '.js-switch-work', function () {
+    var work_id = Number( $('.work-showcase.-shown').parent().attr( 'work_id' ) );
     var next_work_id = work_id;
     var number_of_works = $('.work').length;
 
-    if( $(this).hasClass("js-switch-work--previous") && work_id > 0 ) {
+    if ( $( this ).hasClass( '-previous' ) && work_id > 0 ) {
       next_work_id--;
-      workTracking(next_work_id, 'swich/backwards');
-    } else if( $(this).hasClass("js-switch-work--next") && work_id < (number_of_works - 1) ) {
+      workTracking( next_work_id, 'swich/backwards' );
+    } else if ( $( this ).hasClass( '-next' ) && work_id < ( number_of_works - 1 ) ) {
       next_work_id++;
-      workTracking(next_work_id, 'swich/forwards');
+      workTracking( next_work_id, 'swich/forwards' );
     }
-    switchWork(work_id, next_work_id);
-  });
+    switchWork( work_id, next_work_id );
+  } );
 
-  $(document).on("click", ".js-contact-link a", function () {
-    var link = $(this).html();
-    ga('send', 'event', 'Contacts', 'click', link);
-  });
-});
+  $( document ).on( 'click', '.js-contact-link a', function () {
+    ga( 'send', 'event', 'Contacts', 'click', $( this ).html() );
+  } );
+} );
