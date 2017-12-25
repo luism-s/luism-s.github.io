06ebb3e38a0da258e40448b32fbd1005b85faa49
Luís Silva <luis@brief.pt>
2017-12-25 21:12:01 +0000

    Clean project

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,9 +1,4 @@
-
-$( function () {
+$(function () {
   'use strict';
-
-  $( document ).on( 'click', '.js-contact-link a', function () {
-    ga( 'send', 'event', 'Contacts', 'click', $( this ).html() );
-  } );
   
-} );
+})();
