d93315214815edab3da41d83015c54ac3d2bea00
Luís Silva <luis@brief.pt>
2017-12-31 17:02:17 +0000

    Remove unsused resources

--- src/assets/scripts/main.js
+++
@@ -1,9 +0,0 @@
-
-$( function () {
-  'use strict';
-
-  $( document ).on( 'click', '.js-contact-link a', function () {
-    ga( 'send', 'event', 'Contacts', 'click', $( this ).html() );
-  } );
-  
-} );
