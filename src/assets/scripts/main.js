4ff68c0be327ba1212e69f8d0f39d671f14647e3
Luís Silva <luismiguel.s91@gmail.com>
2018-04-02 00:13:44 +0100

    Adapt menu for mobile

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  new IScroll('#site-navbar-wrapper', { 
+    scrollX: true, 
+    scrollY: false,
+    mouseWheel: true,
+    freeScroll: true,
+    scrollbars: true,
+    wheelHorizontal: true
+  });
+});
