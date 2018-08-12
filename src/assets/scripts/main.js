e6acb7caa77d66045d16661be258a039d141176a
Luís Silva <luis.m.silva@mail.com>
2018-08-12 14:45:21 +0100

    fix identation

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-  new IScroll('#site-navbar-wrapper', { 
-    scrollX: true, 
-    scrollY: false,
-    mouseWheel: true,
-    freeScroll: true,
-    scrollbars: true,
-    wheelHorizontal: true
-  });
+
+    new IScroll('#site-navbar-wrapper', { 
+        scrollX: true, 
+        scrollY: false,
+        mouseWheel: true,
+        freeScroll: true,
+        scrollbars: true,
+        wheelHorizontal: true
+    });
 });
