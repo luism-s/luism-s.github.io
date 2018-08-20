57049dfda8c0a39552c9c015b77c79eac5f00df9
Luís Silva <luis.m.silva@mail.com>
2018-08-21 00:17:24 +0100

    Improve mobile navbar

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,17 +1,6 @@
 $(document).ready(function() {
-/* 
-    new IScroll('#site-navbar-wrapper', { 
-        scrollX: true, 
-        scrollY: false,
-        mouseWheel: true,
-        freeScroll: true,
-        scrollbars: true,
-        wheelHorizontal: true
-    }); */
-
+    
     $('#navbar-hamburguer').on('click', function(e) {
         $('#navbar').toggleClass('-mobile-closed');
     });
-    
-
 });
